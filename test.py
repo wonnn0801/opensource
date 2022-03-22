e9409dcc843fd6d2fb3e700147f8fe2888994cf2
wonnn0801 <101379674+wonnn0801@users.noreply.github.com>
2022-03-22 15:20:15 +0900

    Update test.py
    과제 #1

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 Heewon Kim
 Touched by user2
+
+1. 코드를 수정 후 Pull Request를 이미 한 차례 요청하였고, 상대방은 아직 Merge(수락)를 하지 않은 상황에서 급하게 추가적으로 수정해야할 내용이 생각나서 다시 중복으로 Pull Request를 하게 되는 경우 어떻게 되는가?
+
 print("Opensource SW")
